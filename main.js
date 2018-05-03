c1c7f32fafa635b1f6f3f9582bd7c66eebed74a4
Isabel Fang <saeriel@saeriel.local>
2018-05-03 17:44:05 +0800

    everything

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log("hello");
+
+function myClick1() {
+	document.getElementById("c1").style.opacity = 100;
+}
+function myClick2() {
+	document.getElementById("c2").style.opacity = 100;
+}
+function myClick3() {
+	document.getElementById("c3").style.opacity = 100;
+}
+
+function myClick4() {
+	document.getElementById("c4").style.opacity = 100;
+}
+function myClick5() {
+	document.getElementById("c5").style.opacity = 100;
+}
+function myClick6() {
+	document.getElementById("c6").style.opacity = 100;
+}
+
+function myClick7() {
+	document.getElementById("c7").style.opacity = 100;
+}
+function myClick8() {
+	document.getElementById("c8").style.opacity = 100;
+}
+function myClick9() {
+	document.getElementById("c9").style.opacity = 100;
+}
+
+function myClick10() {
+	document.getElementById("c10").style.opacity = 100;
+}
+function myClick11() {
+	document.getElementById("c11").style.opacity = 100;
+}
+function myClick12() {
+	document.getElementById("c12").style.opacity = 100;
+}
+
+function myClick13() {
+	document.getElementById("c13").style.opacity = 100;
+}
+
+function myClick14() {
+	document.getElementById("c14").style.opacity = 100;
+}
+function myClick15() {
+	document.getElementById("c15").style.opacity = 100;
+}
+function myClick16() {
+	document.getElementById("c16").style.opacity = 100;
+}
